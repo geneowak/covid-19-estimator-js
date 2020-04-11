be4893be9412e78b89185bb074eb135909d4988e
geneowak <teugeneowak@gmail.com>
2020-04-11 10:14:23 +0300

    Complete challenge 1.

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable operator-linebreak */
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, timeToElapse, periodType } = data;
+  let noOfDays = 0;
+  switch (periodType) {
+    case 'days':
+      noOfDays = timeToElapse;
+      break;
+    case 'weeks':
+      noOfDays = timeToElapse * 7;
+      break;
+    case 'months':
+      noOfDays = timeToElapse * 30;
+      break;
+    default:
+      throw new Error('Period type not supported.');
+  }
+  const factor = Math.floor(noOfDays / 3);
+  const projectionMultiplier = 2 ** factor;
+  const impact = {};
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime =
+    impact.currentlyInfected * projectionMultiplier;
+
+  const severeImpact = {};
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * projectionMultiplier;
+
+  return { data, impact, severeImpact };
+};
 
 export default covid19ImpactEstimator;
