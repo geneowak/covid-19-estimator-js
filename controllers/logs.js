c48b0a367dbdb2c1bc7ea288805cbc1f62a5e231
geneowak <teugeneowak@gmail.com>
2020-04-14 13:00:38 +0300

    update log Content-Type

--- controllers/logs.js
+++ controllers/logs.js
@@ -4,6 +4,6 @@ const path = require('path');
 const logFile = 'logs/requests.log';
 
 exports.getLogs = (req, resp, next) => {
-  resp.type('plain/text');
+  resp.type('text/plain');
   resp.sendFile(path.resolve(logFile));
 };
