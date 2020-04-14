fe9bc08f9cde8e7888500f34c44b3a91e65f30b1
geneowak <teugeneowak@gmail.com>
2020-04-14 13:54:40 +0300

    update log content-type

--- controllers/logs.js
+++ controllers/logs.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const logFile = 'logs/requests.log';
 
 exports.getLogs = (req, resp, next) => {
-  resp.type('text/plain');
+  resp.type('plain/text');
   // resp.sendFile(path.resolve(logFile));
   fs.readFile(path.resolve(logFile), (e, data) => {
     if (e) throw e;
