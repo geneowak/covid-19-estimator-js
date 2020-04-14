a522f29bcc45830035f8a245af7e261e5387500f
geneowak <teugeneowak@gmail.com>
2020-04-14 06:53:51 +0300

    update log endpoint

--- controllers/logs.js
+++ controllers/logs.js
@@ -4,5 +4,6 @@ const path = require('path');
 const logFile = 'logs/requests.log';
 
 exports.getLogs = (req, resp, next) => {
+  resp.type('plain/text');
   resp.sendFile(path.resolve(logFile));
 };
